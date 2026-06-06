7820011abe951e1f867993a656d8c0a7126feb55
Utofarman1 <uto.farman@gmail.com>
2026-06-06 13:05:06 +0330

    Add blocks registry

---
+++ src/main/java/com/utofarman/zozi/ZoziBlocks.java
@@ -0,0 +1,12 @@
+package com.utofarman.zozi;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.level.block.Block;
+
+public class ZoziBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(
+            ForgeRegistries.BLOCKS, ZoziMod.MOD_ID);
+    
+    // Blocks will be added here as needed
+}
