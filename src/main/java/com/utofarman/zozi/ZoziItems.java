350690b595675d31637a767b04c64a3e48a102fc
Utofarman1 <uto.farman@gmail.com>
2026-06-06 13:04:46 +0330

    Add items registry

---
+++ src/main/java/com/utofarman/zozi/ZoziItems.java
@@ -0,0 +1,12 @@
+package com.utofarman.zozi;
+
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.item.Item;
+
+public class ZoziItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
+            ForgeRegistries.ITEMS, ZoziMod.MOD_ID);
+    
+    // Items will be added here as needed
+}
